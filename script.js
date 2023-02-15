7c183d5570f412360e3233c3d495acef5572016a
dhanraj-saur <dhananjayverma301@gmail.com>
2023-02-15 14:20:48 +0530

    solve

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+var todoValue = document.getElementById("newTodoInput");
+var btnv = document.getElementById("addTodoBtn");
+var ol = document.getElementById("odoList");
 
+function addTodo (){
+    if(todoValue.value){
+        var li = document.createElement('li')
+        li.textContent = todoValue.value;
+        ol.appendChild('li');
+        todoValue.value = "";
+    }
+}
+
+btnv.addEventListener('click',addTodo)
